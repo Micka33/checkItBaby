e030f63c689d9009757f3e351347b4ad5cd97a49
Mickael <mickael.cassy@gmail.com>
2014-03-03 19:40:36 +0100

    added installation steps

---
+++ checkitbaby.py
@@ -0,0 +1,11 @@
+'brew update && brew install boost && brew install rethinkdb'
+'curl https://npmjs.org/install.sh | sh'
+'npm install casperjs'
+'npm install moment'
+'npm install rethinkdb'
+'brew install protobuf'
+# The JavaScript driver can take advantage of the C++ protocol buffer backend for faster performance.
+#libprotobuf-dev
+# sudo apt-get install libprotobuf-dev
+# On platforms other than Ubuntu, if the package manager does not have the protobuf development files, they can be downloaded from http://code.google.com/p/protobuf/downloads/list.
+'rethinkdb --daemon -d /Users/`whoami`/.checkItBabyDb'
