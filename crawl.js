9968b74959c8910aaa817e5cbc7777a67fd810c0
Mickael <mickael.cassy@gmail.com>
2014-03-16 18:31:45 +0100

    implementing with redis

--- crawl.js
+++ crawl.js
@@ -4,32 +4,21 @@
 var email = 'c29waGllcmV2ZXVzZUB5YWhvby5jb20=';
 var pwd = 'VGVzdDMzMzM=';
 var sParProfile = 60;
+var maxDelayPerRequest = 15;
 
 var page    = require('webpage').create(),
     system  = require('system'),
     args    = require('minimist')(system.args.slice(1)),
-    moment  = require('moment');
-    // redb    = require('rethinkdb');
+    moment  = require('moment'),
+    redis   = require("redis"),
+    uuid    = require('node-uuid');
+    //clearing minimist memory from the pwd and email
+    require('minimist')(['stuff', 'nuff', 'nop', 'top', 'nutch', 'fetch', 'ornot']);
 
-//clearing minimist memory from the pwd and email
-require('minimist')(['stuff', 'nuff', 'nop', 'top', 'nutch', 'fetch', 'ornot']);
 
-// if (((args.password == undefined) && (args.p == undefined)) ||
-//     ((args.email == undefined) && (args.e == undefined)))
-// {
-//   console.log('Usage: crawl.js --password secret --email you@email.com');
-//   console.log('       crawl.js -p secret -e you@email.com');
-//   phantom.exit();
-// }
-// email = btoa((args.email != undefined)?(args.email):(args.e));
-// pwd = btoa((args.password != undefined)?(args.password):(args.p));
 
-//Clearing args parameter from memory and freezing it, since we won't use it anymore.
-args.email = 'nothingtoseeherejustclearing1thistobesurenoonereaditinmemory';
-args.e = 'nothingtoseeherejustclearingthis2tobesurenoonereaditinmemory';
-args.password = 'nothingtoseeherejustclear3ingthistobesurenoonereaditinmemory';
-args.p = 'nothingtoseeherejustclearingthis4tobesurenoonereaditinmemory';
-Object.freeze(args);
+
+
 
 
 
@@ -45,11 +34,21 @@ var log = function(msg)
   var time = moment().format('h:mm:ss a');
   console.log('['+time+'] '+msg);
 };
-var click = function(selector)
+var click = function(selector, fakeMode)
 {
-  page.evaluate(function() {
-    $(selector).click();
-  });
+  if (fakeMode === true)
+  {
+    page.evaluate(function(selector) {
+      return $(selector).trigger('click');
+    }, selector);
+  }
+  else
+  {
+    var position = page.evaluate(function(selector) {
+      return $(selector).offset();
+    }, selector);
+    page.sendEvent("click", position.left, position.top, 'left');
+  }
 };
 var fillAndsubmit = function(form, names) {
   page.evaluate(function(form, names)
@@ -63,15 +62,13 @@ var fillAndsubmit = function(form, names) {
         }).prop('selected', true);
       else if ($(selector).is('input'))
         $(selector).val(names[i][1]);
-      console.log($(selector).val());
     };
     $(form).submit();
-    console.log('form submitted');
   }, form, names);
 };
 var printScreen = function(name)
 {
-  page.render(name+'.jpeg');
+  page.render(name+'.jpeg', {format: 'jpeg', quality: '100'});
 };
 var currentUrl = function() {
   return page.evaluate(function(){return window.location.href;});
@@ -79,28 +76,40 @@ var currentUrl = function() {
 var waittil = function(urlToWaitFor, delay, then) {
   urlLoaded = null;
   var start = moment();
+  var urls = ( (Array.isArray(urlToWaitFor)) ? (urlToWaitFor) : ([urlToWaitFor]) );
   var intervalID = window.setInterval(function()
   {
     if ((moment().diff(start, 'seconds') > delay) ||
-      (urlLoaded === urlToWaitFor))
+      (urls.indexOf(urlLoaded) > -1)/* ||
+      (urls.indexOf(currentUrl()) > -1)*/)
     {
       clearInterval(intervalID);
+      var found = ((urls.indexOf(urlLoaded) > -1)/* || ((urls.indexOf(currentUrl())) > -1) */);
       urlLoaded = null;
-      then();
+      then(found);
     }
   },
   200);
 };
-page.onLoadFinished = function() {
+page.onLoadFinished = function(status) {
   urlLoaded = currentUrl();
 };
 var hasClass = function(el, classToCheck) {
   return page.evaluate(function(el, classToCheck) {
     return $(el).hasClass(classToCheck);
-  },
-  el, classToCheck);
+  }, el, classToCheck);
 };
-
+var hasDiv = function(selector) {
+  var res = page.evaluate(function(selector) {
+    return $(selector).length > 1;
+  }, selector);
+  return res;
+};
+var exit = function() {
+  log('Fin.');
+  redis.quit()
+  phantom.exit();
+};
 
 // page.onUrlChanged = function(targetUrl) {
 //   console.log('New URL: ' + targetUrl);
@@ -112,87 +121,119 @@ var hasClass = function(el, classToCheck) {
 //   console.log('Sent from the page\'s main frame: ' + main);
 // };
 page.onConsoleMessage = function(msg, lineNum, sourceId) {
-  console.log('CONSOLE: ' + msg + ' (from line #' + lineNum + ' in "' + sourceId + '")');
+  // console.log('CONSOLE: ' + msg + ' (from line #' + lineNum + ' in "' + sourceId + '")');
 };
 //
 //
 //
 var getPageResult = function()
 {
-    var data = {profiles:[], total:0};
-    $("#grid-container > .person.large").each(function(index, element)
-    {
-      data.profiles.push($(element).attr('data-id'));
+  return page.evaluate(function() {
+    var profiles = [];
+    $("#grid-container > .person.large").each(function(index, element) {
+      profiles.push({ id: $(element).attr('data-id'),
+                      url: $(element).find('a').attr('href')
+                    });
     });
-    data.total = $('.nav-pager .pager-total:first').text();
-    return JSON.stringify(data);
+    return profiles;
+  });
 };
 var getGuyInfo = function()
 {
-    return JSON.stringify({
-      name: $('#profile-infos .title').text(),
-      age: $('#profile-infos .age').text(),
-      city: $('#profile-infos .city').text()
-    });
+  return JSON.stringify({
+    name: $('#profile-infos .title').text(),
+    age: $('#profile-infos .age').text(),
+    city: $('#profile-infos .city').text()
+  });
 };
 var getPaging = function()
 {
-    return JSON.stringify({
-      pagging: $('#charms strong:first').text()+' sur '+$('#charms strong.pager-total:first').text()
-    });
+  return page.evaluate(function() {
+    return {  nbResults: $('#charms strong:first').text(),
+              totalResults: $('#charms strong.pager-total:first').text()
+            };
+  });
 };
 
 
-// log(redb.protobuf_implementation);
+
+
+
+
+
+
+
+
+
+
+
+
+// if (((args.password == undefined) && (args.p == undefined)) ||
+//     ((args.email == undefined) && (args.e == undefined)))
+// {
+//   console.log('Usage: crawl.js --password secret --email you@email.com');
+//   console.log('       crawl.js -p secret -e you@email.com');
+//   exit();
+// }
+// email = btoa((args.email != undefined)?(args.email):(args.e));
+// pwd = btoa((args.password != undefined)?(args.password):(args.p));
+
+//Clearing args parameter from memory and freezing it, since we won't use it anymore.
+args.email = 'nothingtoseeherejustclearing1thistobesurenoonereaditinmemory';
+args.e = 'nothingtoseeherejustclearingthis2tobesurenoonereaditinmemory';
+args.password = 'nothingtoseeherejustclear3ingthistobesurenoonereaditinmemory';
+args.p = 'nothingtoseeherejustclearingthis4tobesurenoonereaditinmemory';
+Object.freeze(args);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //
 // INIT DATABASE
 //
-// var conn = null;
-// var initDb = function()
-// {
-//   var createdbtable = function(err, tlist) {
-//     log('createdbtable');
-//     if (err) throw err;
-//     log('end-createdbtable');
-//   };
-//   var listdbtable = function(err, tlist) {
-//     log('listdbtable');
-//     if (err) throw err;
-//     if (list.indexOf('people') < 0)
-//     {
-//       redb.db(email).tableCreate('people').run(conn, createdbtable);
-//     }
-//     log('end-listdbtable');
-//   };
-//   var createdb = function(err) {
-//     log('createdb');
-//     if (err) throw err;
-//       redb.db(email).tableList().run(conn, listdbtable);
-//     log('end-createdb');
-//   };
-//   var listdbs = function(err, dblist) {
-//     log('listbds');
-//     if (err) throw err;
-//     if (dblist.indexOf(email) < 0)
-//     {
-//       redb.dbCreate(email).run(conn, createdb);
-//     }
-//     else
-//     {
-//       redb.db(email).tableList().run(conn, listdbtable);
-//     }
-//     log('end-listdbs');
-//   };
-//   var connect = function(err, c) {
-//     log('connect');
-//     if (err) throw err;
-//     conn = c;
-//     redb.dbList().run(conn, listdbs);
-//     log('end-connect');
-//   };
-//   redb.connect( {host: 'localhost', port: 28015}, connect);
-// }
+redis = redis.createClient();
+redis.on('error', function (err) {
+  log('La connection à la bdd a échoué: ' + err);
+  exit();
+});
+redis.on('connect', function () {
+  log('La connection à la bdd a réussie.');
+});
+redis.on('ready', function () {
+  log('La bdd est prête à recevoir les urls des profiles.');
+});
+redis.on('end', function () {
+  log('La connection à la bdd a été coupé.');
+});
+//DATABASE UTILITIES
+var saveProfile = function(profile) {
+  var key = "profiles."+email;
+  var field = profile.id;
+  log('redis.hsetnx('+key+', '+field+', {url:'+profile.url+', checked:false});');
+  // var entryIsNew = redis.hsetnx(key, field, {url:profile.url, checked:false});
+  // if (entryIsNew)
+  //   redis.publish(key, {url:profile.url});
+};
+
+
+
+
+
+
+
 
 
 
@@ -200,63 +241,75 @@ var getPaging = function()
 //
 // Algo
 //
-// var nextPage = function()
-// {
-//   var selector = '#charms .nav-pager a.pager-next';
-//   this.waitForSelector(selector, function()
-//   {
-//     this.click(selector);
-//     this.wait(6000, visitProfiles);
-//   });
-// }
-// var visit = function()
-// {
-//     current++;
-//     subtotal--;
-//     var guy = JSON.parse(this.evaluate(getGuyInfo));
-//     log('Visite de '+guy.name+' / '+guy.age+' / '+guy.city+' ('+this.getCurrentUrl()+')');
-//     this.captureSelector(current.toString()+'_'+guy.name.replace(/\/|\\/, '')+'.jpeg', '#content .content-wrapper', {format: 'jpeg', quality: 100});
-// }
-// var visitProfiles = function()
-// {
-//   log('Affichage des profiles '+JSON.parse(this.evaluate(getPaging)).pagging+'.');
-//   var ctx = this;
-//   var datas = JSON.parse(this.evaluate(getPageResult));
-//   total = datas.total;
-//   subtotal = datas.profiles.length;
-//   var i = 0;
-//   this.repeat(datas.profiles.length, function repeat()
-//   {
-//     var url = atob('aHR0cDovL3d3dy5hZG9wdGV1bm1lYy5jb20vcHJvZmlsZS8=')+datas.profiles[i].toString();
-//     this.thenOpen(url, visit);
-//     this.wait((sParProfile*1000), function()
-//     {
-//       this.back();
-//     });
-//     i++;
-//   });
-//   this.then(nextPage);
-// }
-// var getSearchResults = function()
-// {
-//   log('Récupération de la liste des profiles.');
-//   var hasResults = this.evaluate(function(selector)
-//   {
-//     return $(selector).length == 1;
-//   }, 'section#charms');
-//   if (!hasResults)
-//     this.click('form#search-form button[type="submit"]');//Clicking on the search button
-//   this.wait(6000, visitProfiles);//waiting 6s
-// }
+var visitProfiles = function()
+{
+  var paging = getPaging();
+  log('Affichage des profiles '+paging.nbResults+' sur '+paging.totalResults+'.');
+  var profiles = getPageResult();
+  log('length:'+profiles.length.toString());
+  log('profiles:'+profiles);
+
+  for (var i = profiles.length - 1; i >= 0; i--) {
+    saveProfile(profiles[i]);
+  };
+
+  if (hasClass('#charms .nav-pager a.pager-next span.pager-right', 'off'))
+  {
+    log('Tous les resultats ont été récupéré.');
+    exit();
+  }
+
+  var curl = currentUrl();
+  var nurl = curl.split('=')[0]+'='+(parseInt(curl.split('=')[1]) + 1);
+  click('#charms .nav-pager a.pager-next', true);
+  waittil(nurl, maxDelayPerRequest, function(loaded)
+  {
+    if (loaded || (currentUrl() === nurl))
+    {
+      visitProfiles();
+    }
+    else
+    {
+      log("Impossible de charger la page suivante des profiles. :'(");
+      exit();
+    }
+  });
+}
+var getSearchResults = function()
+{
+  log('Récupération de la liste des profiles.');
+  // If there is no results displayed
+  if (!hasDiv('section#charms'))
+    fillAndsubmit('form#search-form',[]); //launch a new research
+  waittil('http://www.adopteunmec.com/mySearch/?page=1', maxDelayPerRequest, function(loaded)
+  {
+    if (loaded)
+    {
+      visitProfiles();
+    }
+    else
+    {
+      log("Impossible de charger la liste des profiles. :'(");
+      exit();
+    }
+  });
+}
 var goToSearchPage = function()
 {
-//   log('Affichage de la page de recherche.');
-//   this.wait(6000, function()//waiting 6s
-//   {
-//     this.click('#search a');//Clicking on the search link
-//     this.then(getSearchResults);
-//   });
-  phantom.exit();
+  log('Affichage de la page de recherche.');
+  click('#search a');//Clicking on the search link
+  waittil(['http://www.adopteunmec.com/mySearch/?page=1', 'http://www.adopteunmec.com/mySearch'], maxDelayPerRequest, function(loaded)
+  {
+    if (loaded)
+    {
+      getSearchResults();
+    }
+    else
+    {
+      log('Impossible de charger la page de recherche. :\'(');
+      exit();
+    }
+  });
 }
 var signIn = function()
 {
@@ -267,7 +320,7 @@ var signIn = function()
     [['username', atob(email)],
      ['password', atob(pwd)]
     ]);
-  waittil('http://www.adopteunmec.com/index', 10, function()
+  waittil('http://www.adopteunmec.com/index', maxDelayPerRequest, function()
   {
     if (hasClass('body', 'logged'))
     {
@@ -278,192 +331,9 @@ var signIn = function()
     else
     {
       log('Connexion échouée. :\'(');
-      phantom.exit();
+      exit();
     }
   });
 }
 
 page.open(atob('aHR0cDovL3d3dy5hZG9wdGV1bm1lYy5jb20v'),signIn);
-
-// var signIn = function()
-// {
-//   // initDb();
-//   while(dbNotReady);
-//   log('Connexion en tant que '+atob(email)+'.');
-//   this.fill('form#login',
-//   {
-//       username: atob(email),
-//       password: atob(pwd)
-//   }, true); //Filling log in form and submiting
-//   this.then(goToSearchPage);
-// }
-// casper.start().then(signIn);
-// casper.run(function()
-// {
-//   this.exit();
-// });
-
-
-
-
-
-// age[min]    23
-// age[max]    28
-// age_step    1
-// by  region
-// country fr
-// region  11
-// bsmSelectnice-multiple0
-// subregion[] 76
-// distance[min]
-// distance[max]   0
-// distance_step   10
-// pseudo
-// university-text
-// university
-// sex 0
-// size[min]   135
-// size[max]   185
-// size_step   5
-// weight[min] 35
-// weight[max] 55
-// weight_step 5
-// mandatory[] shape
-// shape[] 1
-// shape[] 2
-// shape[] 3
-// shape[] 4
-// shape[] 5
-// shape[] 6
-// mandatory[] eyes_color
-// eyes_color[]    1
-// eyes_color[]    2
-// eyes_color[]    3
-// eyes_color[]    4
-// eyes_color[]    5
-// eyes_color[]    6
-// mandatory[] origins
-// origins[]   1
-// origins[]   2
-// origins[]   3
-// origins[]   4
-// origins[]   5
-// origins[]   6
-// origins[]   7
-// origins[]   8
-// mandatory[] body_hair
-// body_hair[] 5
-// body_hair[] 1
-// body_hair[] 2
-// body_hair[] 3
-// body_hair[] 4
-// body_hair[] 6
-// mandatory[] hair_color
-// hair_color[]    1
-// hair_color[]    2
-// hair_color[]    10
-// hair_color[]    8
-// hair_color[]    7
-// hair_color[]    5
-// hair_color[]    6
-// hair_color[]    4
-// hair_color[]    3
-// hair_color[]    9
-// mandatory[] hair_size
-// hair_size[] 1
-// hair_size[] 2
-// hair_size[] 3
-// hair_size[] 4
-// mandatory[] hair_style
-// hair_style[]    1
-// hair_style[]    2
-// hair_style[]    3
-// hair_style[]    4
-// hair_style[]    5
-// hair_style[]    6
-// hair_style[]    7
-// hair_style[]    8
-// mandatory[] styles
-// styles[]    7
-// styles[]    12
-// styles[]    3
-// styles[]    2
-// styles[]    14
-// styles[]    15
-// styles[]    6
-// styles[]    16
-// styles[]    17
-// styles[]    18
-// styles[]    19
-// styles[]    20
-// styles[]    10
-// styles[]    21
-// styles[]    22
-// styles[]    8
-// styles[]    24
-// styles[]    25
-// styles[]    26
-// styles[]    27
-// styles[]    4
-// styles[]    5
-// styles[]    28
-// styles[]    11
-// styles[]    29
-// styles[]    30
-// styles[]    31
-// styles[]    32
-// styles[]    33
-// styles[]    34
-// styles[]    9
-// styles[]    35
-// styles[]    36
-// styles[]    1
-// mandatory[] features
-// features[]  1
-// features[]  2
-// features[]  3
-// features[]  4
-// features[]  6
-// features[]  7
-// features[]  8
-// features[]  5
-// mandatory[] skills
-// mandatory[] accessories
-// mandatory[] transport
-// mandatory[] diet
-// diet[]  1
-// diet[]  2
-// diet[]  3
-// diet[]  4
-// diet[]  5
-// mandatory[] favourite_food
-// favourite_food[]    1
-// favourite_food[]    2
-// favourite_food[]    3
-// favourite_food[]    4
-// favourite_food[]    5
-// favourite_food[]    6
-// favourite_food[]    7
-// favourite_food[]    8
-// favourite_food[]    9
-// favourite_food[]    10
-// favourite_food[]    11
-// favourite_food[]    12
-// favourite_food[]    13
-// favourite_food[]    14
-// favourite_food[]    15
-// favourite_food[]    16
-// mandatory[] tobacco
-// tobacco[]   1
-// tobacco[]   2
-// tobacco[]   3
-// tobacco[]   5
-// tobacco[]   4
-// tobacco[]   6
-// mandatory[] alcohol
-// alcohol[]   1
-// alcohol[]   2
-// alcohol[]   5
-// alcohol[]   3
-// alcohol[]   4
-// alcohol[]   6
